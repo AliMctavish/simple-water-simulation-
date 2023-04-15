f4c9d43202e433a53c1c630d21b1abe983a72952
ali fadil <alifadil4141@gmail.com>
2023-04-15 17:23:53 +0300

    more preformance added

--- sfmlTest/Game.cpp
+++ sfmlTest/Game.cpp
@@ -12,8 +12,8 @@ void Game::initVariables()
 
 void Game::initEnemies()
 {
-	this->enemy.setPosition(this->enemyPosX,this->enemyPosY);
-	this->enemy.setSize(sf::Vector2f(150.f,50.f));
+	this->enemy.setPosition(this->enemyPosX, this->enemyPosY);
+	this->enemy.setSize(sf::Vector2f(150.f, 50.f));
 	this->enemy.setFillColor(sf::Color::Red);
 	this->enemy.setOutlineColor(sf::Color::White);
 	this->enemy.setOutlineThickness(1.f);
@@ -81,6 +81,28 @@ void Game::Controllers()
 		platform.inIt(sf::Mouse::getPosition(*this->window).x, sf::Mouse::getPosition(*this->window).y);
 		objects.push_back(platform);
 	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
+	{
+		enemies.clear();
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::G))
+	{
+		objects.clear();
+	}
+
+}
+void Game::Update()
+{
+	this->pollEvents();
+	this->Controllers();
+	for (int i = 0; i < enemies.size(); i++)
+	{
+		enemies[i].Update();
+		if (enemies[i].enemyTexture.getPosition().y > this->window->getSize().y)
+		{
+			enemies.erase(enemies.begin() + i);
+		}
+	}
 	for (int i = 0; i < objects.size(); i++)
 	{
 		for (int j = 0; j < enemies.size(); j++)
@@ -91,14 +113,9 @@ void Game::Controllers()
 			}
 		}
 	}
-}
-void Game::Update()
-{ 
-	this->pollEvents();
-	this->Controllers();
 	//Update Mouse Position 
-	std::cout << "Mouse position : " << sf::Mouse::getPosition(*this->window).x << " " << sf::Mouse::getPosition(*this->window).y << std::endl;
-	std::cout << "rectangle position : " << this->enemy.getPosition().x << " " << this->enemy.getPosition().y << std::endl;
+	//std::cout << "Mouse position : " << sf::Mouse::getPosition(*this->window).x << " " << sf::Mouse::getPosition(*this->window).y << std::endl;
+	//std::cout << "rectangle position : " << this->enemy.getPosition().x << " " << this->enemy.getPosition().y << std::endl;
 }
 
 void Game::render()
@@ -114,18 +131,10 @@ void Game::render()
 
 	for (int i = 0; i < enemies.size(); i++)
 	{
-	  enemies[i].Update();
 	  this->window->draw(enemies[i].enemyTexture);
 	}
 
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
-	{
-		enemies.clear();
-	}
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::G))
-	{
-		objects.clear();
-	}
+
 
 	this->window->display();
 }
